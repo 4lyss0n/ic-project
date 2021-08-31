50ade1b70bba5fe30f841efa63ad98e091d51ab9
4lyss0n <alyssonramos3000@gmail.com>
2021-08-30 20:48:25 -0400

    update in main page

--- src/pagesPart/index/page.tsx
+++ src/pagesPart/index/page.tsx
@@ -13,11 +13,11 @@ const Component: FunctionComponent<IProps> = ({ }) => {
                     <h1>Ansiedade e Depressão</h1>
                 </header>
                 <main>
-                    <ContentBlock title={<h2>Objetivos</h2>} initEnable={true}>
-                        Os objetivos de nosso projeto é fornecer apoio a pessoas que sofrem de doenças como ansiedade e depressão.
+                    <ContentBlock title={<h2>Objetivos</h2>} initEnable={false}>
+                        <p>Os objetivos de nosso projeto é fornecer apoio a pessoas que sofrem de doenças como ansiedade e depressão. </p>
                     </ContentBlock>
-                    <ContentBlock title={<h2>Justificativa</h2>} initEnable={true}>
-                        Escolhemos esse tema, porque muitas pessoas sofrem com essas doenças esperamos ajudar essas pessoas de alguma forma. Será um incentivo para essas procurarem ajuda profissional e mostrar que elas não estão sozinhas
+                    <ContentBlock title={<h2>Justificativa</h2>} initEnable={false}>
+                        <p> Escolhemos esse tema, porque muitas pessoas sofrem com essas doenças esperamos ajudar essas pessoas de alguma forma. Será um incentivo para essas procurarem ajuda profissional e mostrar que elas não estão sozinhas </p>
                     </ContentBlock>
                 </main>
                 <footer>
